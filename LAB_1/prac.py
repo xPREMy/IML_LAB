fa60726a1556859b5769cec4ff1dca2db077a087
xPREMy <b24bb1028@iitj.ac.in>
2025-08-12 22:46:09 +0530

    q3part1

--- LAB_1/prac.py
+++ LAB_1/prac.py
@@ -1,8 +1,12 @@
+import pandas as pd
 import numpy as np
-V=np.array([[1,3,5],[3,2,6],[1,7,3]])
-D=np.array([[1,0,0],[0,2,0],[0,0,3]])
+def str_to_num(iris):
+    iris_plant={'Iris-setosa':1,"Iris-versicolor":2,'Iris-virginica':3}
+    return iris_plant[iris]
+df=pd.read_csv("iris.txt")
+df['iris_num']=df["Iris plant"].apply(str_to_num)
 
-B=V @ D @ V.T
-print(B)
-egnval,egnV=np.linalg.eig(B)
-print(np.argsort(egnval)[::-1])
+df=df.drop("Iris plant",axis=1)
+df=df.values.astype("float32")
+cov_matrix=np.cov(df)
+print(cov_matrix)
